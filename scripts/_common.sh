c7f83c6c3b05b175e02caf54eedcc62d8ad35175
Patrick <49989644+botagas@users.noreply.github.com>
2025-02-19 03:18:35 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,6 +11,26 @@ app=janitorr
 leaving_soon_dir="/home/yunohost.multimedia/share/leavingsoon"
 media_server_leaving_soon_dir="/home/yunohost.multimedia/share/leavingsoon"
 
+if ls /etc/yunohost/apps | grep -q "radarr"; then
+        radarr_port=$(ynh_app_setting_get --app radarr --key port)
+        radarr_path=$(ynh_app_setting_get --app radarr --key path)
+        radarr_url="http://$radarr_path:$radarr_port"
+        radarr_api_key=$(ynh_app_setting_get --app radarr --key api_key)
+fi
+if ls /etc/yunohost/apps | grep -q "sonarr"; then
+        sonarr_port=$(ynh_app_setting_get --app sonarr --key port)
+        sonarr_path=$(ynh_app_setting_get --app sonarr --key path)
+        sonarr_url="http://$sonarr_path:$sonarr_port"
+        sonarr_api_key=$(ynh_app_setting_get --app sonarr --key api_key)
+fi
+if ls /etc/yunohost/apps | grep -q "jellystat"; then
+        jellystat_port=$(ynh_app_setting_get --app sonarr --key port)
+        jellystat_path=$(ynh_app_setting_get --app sonarr --key path)
+        jellystat_url="http://$sonarr_path:$sonarr_port"
+        
+        jellystat_api_key=$(ynh_app_setting_get --app sonarr --key api_key)
+fi
+
 #=================================================
 # COMMON FUNCTIONS
 #=================================================
