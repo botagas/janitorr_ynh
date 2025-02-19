72351208236ff5915f43f31379c1fb2c2cd3448e
Patrick <49989644+botagas@users.noreply.github.com>
2025-02-19 03:33:27 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,21 +15,36 @@ if ls /etc/yunohost/apps | grep -q "radarr"; then
         radarr_port=$(ynh_app_setting_get --app radarr --key port)
         radarr_url="http://127.0.0.1:$radarr_port"
         radarr_api_key=$(ynh_app_setting_get --app radarr --key api_key)
+else
+        radarr_port=""
+        radarr_api_key=""
 fi
 if ls /etc/yunohost/apps | grep -q "sonarr"; then
         sonarr_port=$(ynh_app_setting_get --app sonarr --key port)
         sonarr_url="http://127.0.0.1:$sonarr_port"
         sonarr_api_key=$(ynh_app_setting_get --app sonarr --key api_key)
 fi
+else
+        sonarr_port=""
+        sonarr_api_key=""
+fi
 if ls /etc/yunohost/apps | grep -q "jellystat"; then
         jellystat_port=$(ynh_app_setting_get --app jellystat --key port)
         jellystat_url="http://127.0.0.1:$jellystat_port"
         jellystat_api_key=$(ynh_app_setting_get --app jellystat --key api_key)
 fi
+else
+        jellystat_port=""
+        jellystat_api_key=""
+fi
 if ls /etc/yunohost/apps | grep -q "jellyseerr"; then
         jellyseerr_port=$(ynh_app_setting_get --app jellyseerr --key port)
         jellyseerr_url="http://127.0.0.1:$jellyseerr_port"
         jellyseerr_api_key=$(ynh_app_setting_get --app jellyseerr --key api_key)
+fi
+else
+        jellyseerr_port=""
+        jellyseerr_api_key=""
 fi
 
 #=================================================
