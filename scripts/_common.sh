78c011257c0f256636d6cc13412a2a707125dd88
Patrick <49989644+botagas@users.noreply.github.com>
2025-03-13 20:04:07 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,6 +14,31 @@ media_server_leaving_soon_dir="/home/yunohost.multimedia/share/leavingsoon"
 #=================================================
 # COMMON FUNCTIONS
 #=================================================
+# Ensure all variables are initialized with user-provided values. This is to ensure Janitorr runs, because even if a service is set to false, it checks for empty variables.
+radarr_port="${radarr_port:1234}"
+radarr_url="${radarr_url:http://example.tld}"
+radarr_api_key="${radarr_api_key:examplestring}"
+
+bazarr_port="${bazarr_port:1234}"
+bazarr_url="${bazarr_url:http://example.tld}"
+bazarr_api_key="${bazarr_api_key:examplestring}"
+
+sonarr_port="${sonarr_port:1234}"
+sonarr_url="${sonarr_url:http://example.tld}"
+sonarr_api_key="${sonarr_api_key:examplestring}"
+
+jellystat_port="${jellystat_port:1234}"
+jellystat_url="${jellystat_url:http://example.tld}"
+jellystat_api_key="${jellystat_api_key:examplestring}"
+
+jellyfin_port="${jellyfin_port:1234}"
+jellyfin_url="${jellyfin_url:http://example.tld}"
+
+jellyseerr_port="${jellyseerr_port:1234}"
+jellyseerr_url="${jellyseerr_url:http://example.tld}"
+jellyseerr_api_key="${jellyseerr_api_key:examplestring}"
+jellyfin_api_key="${jellyfin_api_key:examplestring}"
+
 # Find service API keys and ports
 if ls /etc/yunohost/apps | grep -q "radarr"; then
         radarr_port=$(ynh_app_setting_get --app radarr --key port)
